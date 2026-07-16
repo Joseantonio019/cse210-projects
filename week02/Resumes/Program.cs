6fcd71ea277aebb5d592e893f7811ef611f43fdc
Joseantonio019 <joseantoniorodriguezcoa@gmail.com>
2026-07-16 18:36:13 -0400

    Add files via upload

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,32 @@
 using System;
 
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // First job
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Data Analyst";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        // Second job
+        Job job2 = new Job();
+        job2._company = "Google";
+        job2._jobTitle = "Data Analyst";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        // Resume
+        Resume myResume = new Resume();
+        myResume._name = "Michael Wolf";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Display resume
+        myResume.Display();
     }
 }
