f390499576cf5fe06a6206ee8bb8b9f37100dc7e
Joseantonio019 <joseantoniorodriguezcoa@gmail.com>
2026-08-13 19:26:27 -0400

    Add files via upload

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,205 +1,101 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 
 // Base class
-public abstract class Goal
+public abstract class Activity
 {
-    protected string _name;
-    protected int _points;
-    protected bool _isComplete;
+    private DateTime _date;
+    private int _minutes;
 
-    public Goal(string name, int points)
+    public Activity(DateTime date, int minutes)
     {
-        _name = name;
-        _points = points;
-        _isComplete = false;
+        _date = date;
+        _minutes = minutes;
     }
 
-    public abstract int RecordEvent();
-    public abstract string GetStatus();
-    public abstract string GetStringRepresentation();
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    public abstract double GetDistance(); // km or miles
+    public abstract double GetSpeed();    // kph or mph
+    public abstract double GetPace();     // min per km or mile
+
+    public virtual string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {GetType().Name} ({Minutes} min): " +
+               $"Distance {GetDistance():F1}, Speed {GetSpeed():F1}, Pace {GetPace():F2}";
+    }
 }
 
-// Simple goal
-public class SimpleGoal : Goal
+// Running class
+public class Running : Activity
 {
-    public SimpleGoal(string name, int points) : base(name, points) { }
+    private double _distance; // km or miles
 
-    public override int RecordEvent()
+    public Running(DateTime date, int minutes, double distance)
+        : base(date, minutes)
     {
-        if (!_isComplete)
-        {
-            _isComplete = true;
-            return _points;
-        }
-        return 0;
+        _distance = distance;
     }
 
-    public override string GetStatus() => _isComplete ? "[X]" : "[ ]";
+    public override double GetDistance() => _distance;
 
-    public override string GetStringRepresentation() => $"SimpleGoal:{_name},{_points},{_isComplete}";
+    public override double GetSpeed() => (_distance / Minutes) * 60;
+
+    public override double GetPace() => Minutes / _distance;
 }
 
-// Eternal goal
-public class EternalGoal : Goal
+// Cycling class
+public class Cycling : Activity
 {
-    public EternalGoal(string name, int points) : base(name, points) { }
+    private double _speed; // kph or mph
 
-    public override int RecordEvent() => _points;
+    public Cycling(DateTime date, int minutes, double speed)
+        : base(date, minutes)
+    {
+        _speed = speed;
+    }
 
-    public override string GetStatus() => "[∞]";
+    public override double GetDistance() => (_speed * Minutes) / 60;
 
-    public override string GetStringRepresentation() => $"EternalGoal:{_name},{_points}";
+    public override double GetSpeed() => _speed;
+
+    public override double GetPace() => 60 / _speed;
 }
 
-// Checklist goal
-public class ChecklistGoal : Goal
+// Swimming class
+public class Swimming : Activity
 {
-    private int _targetCount;
-    private int _currentCount;
-    private int _bonus;
+    private int _laps;
 
-    public ChecklistGoal(string name, int points, int targetCount, int bonus)
-        : base(name, points)
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
     {
-        _targetCount = targetCount;
-        _bonus = bonus;
-        _currentCount = 0;
+        _laps = laps;
     }
 
-    public override int RecordEvent()
-    {
-        _currentCount++;
-        if (_currentCount >= _targetCount)
-        {
-            _isComplete = true;
-            return _points + _bonus;
-        }
-        return _points;
-    }
+    public override double GetDistance() => _laps * 50 / 1000.0; // km
 
-    public override string GetStatus() => $"Completed {_currentCount}/{_targetCount}";
+    public override double GetSpeed() => (GetDistance() / Minutes) * 60;
 
-    public override string GetStringRepresentation() =>
-        $"ChecklistGoal:{_name},{_points},{_targetCount},{_bonus},{_currentCount},{_isComplete}";
+    public override double GetPace() => Minutes / GetDistance();
 }
 
-// Main program class
-public class EternalQuestProgram
-{
-    private List<Goal> _goals = new List<Goal>();
-    private int _score = 0;
-
-    public void CreateGoal()
-    {
-        Console.WriteLine("Choose goal type: 1) Simple  2) Eternal  3) Checklist");
-        int choice = int.Parse(Console.ReadLine());
-
-        Console.Write("Enter goal name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter points: ");
-        int points = int.Parse(Console.ReadLine());
-
-        switch (choice)
-        {
-            case 1:
-                _goals.Add(new SimpleGoal(name, points));
-                break;
-            case 2:
-                _goals.Add(new EternalGoal(name, points));
-                break;
-            case 3:
-                Console.Write("Enter required times to complete: ");
-                int target = int.Parse(Console.ReadLine());
-                Console.Write("Enter bonus points: ");
-                int bonus = int.Parse(Console.ReadLine());
-                _goals.Add(new ChecklistGoal(name, points, target, bonus));
-                break;
-        }
-    }
-
-    public void RecordEvent()
-    {
-        ShowGoals();
-        Console.Write("Select the number of the goal you accomplished: ");
-        int index = int.Parse(Console.ReadLine()) - 1;
-        _score += _goals[index].RecordEvent();
-        Console.WriteLine($"Event recorded! Total score: {_score}");
-    }
-
-    public void ShowGoals()
-    {
-        Console.WriteLine("\n--- Goals List ---");
-        for (int i = 0; i < _goals.Count; i++)
-        {
-            Console.WriteLine($"{i + 1}. {_goals[i].GetStatus()} {_goals[i].GetStringRepresentation()}");
-        }
-    }
-
-    public void SaveGoals()
-    {
-        using (StreamWriter outputFile = new StreamWriter("goals.txt"))
-        {
-            outputFile.WriteLine(_score);
-            foreach (Goal g in _goals)
-                outputFile.WriteLine(g.GetStringRepresentation());
-        }
-    }
-
-    public void LoadGoals()
-    {
-        string[] lines = File.ReadAllLines("goals.txt");
-        _score = int.Parse(lines[0]);
-        _goals.Clear();
-
-        for (int i = 1; i < lines.Length; i++)
-        {
-            string[] parts = lines[i].Split(':');
-            string type = parts[0];
-            string[] data = parts[1].Split(',');
-
-            switch (type)
-            {
-                case "SimpleGoal":
-                    _goals.Add(new SimpleGoal(data[0], int.Parse(data[1])));
-                    break;
-                case "EternalGoal":
-                    _goals.Add(new EternalGoal(data[0], int.Parse(data[1])));
-                    break;
-                case "ChecklistGoal":
-                    _goals.Add(new ChecklistGoal(data[0], int.Parse(data[1]), int.Parse(data[2]), int.Parse(data[3])));
-                    break;
-            }
-        }
-    }
-
-    public void DisplayScore() => Console.WriteLine($"Current score: {_score}");
-}
-
-// Entry point
+// Main program
 class Program
 {
     static void Main()
     {
-        EternalQuestProgram eq = new EternalQuestProgram();
-        bool running = true;
-
-        while (running)
+        List<Activity> activities = new List<Activity>
         {
-            Console.WriteLine("\n1. Create goal\n2. Record event\n3. Show goals\n4. Save goals\n5. Load goals\n6. Display score\n7. Exit");
-            int option = int.Parse(Console.ReadLine());
+            new Running(new DateTime(2022, 11, 3), 30, 4.8),
+            new Cycling(new DateTime(2022, 11, 3), 45, 9.7),
+            new Swimming(new DateTime(2022, 11, 3), 25, 40)
+        };
 
-            switch (option)
-            {
-                case 1: eq.CreateGoal(); break;
-                case 2: eq.RecordEvent(); break;
-                case 3: eq.ShowGoals(); break;
-                case 4: eq.SaveGoals(); break;
-                case 5: eq.LoadGoals(); break;
-                case 6: eq.DisplayScore(); break;
-                case 7: running = false; break;
-            }
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
         }
     }
 }
